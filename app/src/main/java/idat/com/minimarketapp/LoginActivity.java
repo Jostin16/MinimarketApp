1c3e03cc1d29ac3527f4209479e168c8c9fe92e4
Jostin16 <jostinmartinez1605@gmail.com>
2021-04-20 22:58:38 -0500

    cambios

--- app/src/main/java/idat/com/minimarketapp/LoginActivity.java
+++ app/src/main/java/idat/com/minimarketapp/LoginActivity.java
@@ -2,15 +2,27 @@ package idat.com.minimarketapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import idat.com.minimarketapp.databinding.ActivityMainBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         getSupportActionBar().hide();
+
+
     }
+
+
+
 }
